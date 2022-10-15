4fdeb922fd8c47ee08d4910a9c7bfb5d63cc83fc
Cesario <Cesarioneto7@gmail.com>
2022-10-15 11:09:40 -0300

    Organizando templates, criando arquivos estáticos, configurando e carregando os arquivos estáticos

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,8 @@
+from email.mime import image
 from django.urls import path
-from galeria.views import index
+from galeria.views import imagem, index
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('imagem/', imagem)
 ]
